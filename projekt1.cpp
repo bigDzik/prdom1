81ef1869f303acfdd85f1889fe3942e79e594960
Adrian Augustyniak <302451wmil@aleks-2.mat.umk.pl>
2024-03-10 14:07:28 +0100

    pierwsza-zmiana

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+bool czy_pierwsza(int n){
+	if(n<2)
+		return false;
+	
+	for (int i = 2; i*i<=n;i++){
+		if(n%i==0)
+			return false;
+	}
+
+	return true;
+}
+
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
